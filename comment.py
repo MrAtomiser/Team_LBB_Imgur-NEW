0f794e13d539a864addc802fdd9bcaa479051685
draogtech <draogtech@gmail.com>
2017-10-02 17:16:55 +0000

    All updated working classes and methods pushed to new remote repository

---
+++ comment.py
@@ -0,0 +1,9 @@
+class Comment():
+  def __init__(self,user, date_created, image_id, text, score):
+    self.user = user
+    self.date_created = date_created
+    self.image_id = image_id
+    self.text = text
+    self.score = score
+
+  
