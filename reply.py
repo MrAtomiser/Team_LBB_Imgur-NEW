1222a8f52eb1dc6425b724b6640a23259d9d9529
mrtomisin <oluwatomisin@meltwater.org>
2017-10-02 18:46:21 +0000

    Revert "All updated working classes and methods pushed to new remote repository"
    This reverts commit 0f794e13d539a864addc802fdd9bcaa479051685.

--- reply.py
+++
@@ -1,9 +0,0 @@
-class Reply():
-  def __init__(self,comment_id, date_created, text, user_id, score):
-    self.comment_id = comment_id
-    self.date_created = date_created
-    self.text = text
-    self.user_id = user_id
-    self.score = score
-
-
